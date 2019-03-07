844e8d71761d464057813e34d86779c184a49aee
Tim Schumacher <tim@timakro.de>
2019-03-07 21:22:26 +0100

    Version 2.0.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='apispec-oneofschema',
-      version='2.0.0',
+      version='2.0.1',
       license='LGPLv3',
       description='Plugin for apispec providing support for '
                   'Marshmallow-OneOfSchema schemas',
