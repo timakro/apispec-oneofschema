cabe1d4e28d50dd7b1bf1a21a79f932801d7a024
Tim Schumacher <tim@tschumacher.net>
2024-10-28 16:29:04 +0100

    Remove GPL header, bump version

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup
 
 setup(name='apispec-oneofschema',
-      version='3.0.0',
+      version='3.0.1',
       license='MIT',
       description='Plugin for apispec providing support for '
                   'Marshmallow-OneOfSchema schemas',
       author='Tim Schumacher',
-      author_email='tim@timakro.de',
       url='https://github.com/timakro/apispec-oneofschema',
       install_requires=[
           'apispec>=3.0.0',
